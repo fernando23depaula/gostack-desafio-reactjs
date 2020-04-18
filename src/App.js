ae413b7fe9e3ee40d23768ae5da78937e5f99f39
Paulinha <fernando23.depaula@gmail.com>
2020-04-18 19:20:58 -0300

    Front end adicionando e removendo repositorios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from "react";
-
+import React,{useState,useEffect} from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+  const [repositories,setRepositories] = useState([]);
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', { title:`Novo repositorio ${Date.now()}`, url:"New URL",techs:['HTML',"Cobol"] });
+    setRepositories([...repositories,response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+      const repositorie = repositories.filter(repositorie => repositorie.id !== id)
+      return setRepositories(repositorie)
   }
+
+  useEffect(()=>{
+    api.get('repositories')
+    .then(response=> setRepositories(response.data))
+  },[]);
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
+        {repositories.map(repositorie=> <li key={repositorie.id}>
+          {repositorie.title}
+          <button onClick={() => handleRemoveRepository(repositorie.id)}>Remover</button>
         </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
